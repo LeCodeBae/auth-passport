b4ce5f2e651c46d17b26200c25ea205b02e24ca8
LeCodeBae <billivank_96@yahoo.co.id>
2017-04-26 16:46:52 +0700

    auth bisa tapi kode blom rapi

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var jwtHelper = require('../helpers/jwt_helpers');
+var jwt = require('jsonwebtoken');
+var api = require('../controllers/userController')
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+var db = require('../models')
+var app = express()
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log(username)
+    db.User.findOne({where: { username: username }}).then(function (user) {
+      console.log(user)
+      done(null, user)
+    }) ;
+  }
+));
+app.use(passport.initialize());
+/* GET users listing. */
+router.post('/signup', api.signUp)
+router.post('/signin', passport.authenticate('local', {session: false}), api.createToken)
+router.get('/users', jwtHelper.authenticateAdmin, api.getAllUser)
+router.get('/users/:id', jwtHelper.authenticateUser, api.getUser)
+router.post('/users',jwtHelper.authenticateAdmin, api.createUser)
+router.delete('/users/:id',jwtHelper.authenticateAdmin, api.deleteUser)
+router.put('/users/:id',jwtHelper.authenticateUpdate, api.updateUser)
+
+module.exports = router;
